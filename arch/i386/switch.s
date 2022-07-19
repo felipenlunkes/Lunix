798f477298afa9140627de1ae7044da060a5a759
Felipe Lunkes <felipenldev@gmail.com>
2022-07-19 09:36:59 -0300

    Formatação

--- arch/i386/switch.s
+++ arch/i386/switch.s
@@ -6,22 +6,22 @@ switchTask:
     pusha
     pushf
 
-    mov %cr3, %eax #Push CR3
+    mov %cr3, %eax     # Push CR3
 
     push %eax
 
-    mov 44(%esp), %eax #The first argument, where to save
+    mov 44(%esp), %eax # The first argument, where to save
     mov %ebx, 4(%eax)
     mov %ecx, 8(%eax)
     mov %edx, 12(%eax)
     mov %esi, 16(%eax)
     mov %edi, 20(%eax)
-    mov 36(%esp), %ebx #EAX
-    mov 40(%esp), %ecx #IP
-    mov 20(%esp), %edx #ESP
-    add $4, %edx #Remove the return value ;)
-    mov 16(%esp), %esi #EBP
-    mov 4(%esp), %edi #EFLAGS
+    mov 36(%esp), %ebx #E AX
+    mov 40(%esp), %ecx # IP
+    mov 20(%esp), %edx # ESP
+    add $4, %edx       # Remove the return value ;)
+    mov 16(%esp), %esi # EBP
+    mov 4(%esp), %edi  # EFLAGS
     mov %ebx, (%eax)
     mov %edx, 24(%eax)
     mov %esi, 28(%eax)
@@ -34,34 +34,34 @@ switchTask:
     
     push %ebx #Goodbye again ;)
     
-    mov 48(%esp), %eax #Now it is the new object
-    mov 4(%eax), %ebx #EBX
-    mov 8(%eax), %ecx #ECX
-    mov 12(%eax), %edx #EDX
-    mov 16(%eax), %esi #ESI
-    mov 20(%eax), %edi #EDI
-    mov 28(%eax), %ebp #EBP
+    mov 48(%esp), %eax  # Now it is the new object
+    mov 4(%eax), %ebx   # EBX
+    mov 8(%eax), %ecx   # ECX
+    mov 12(%eax), %edx  # EDX
+    mov 16(%eax), %esi  # ESI
+    mov 20(%eax), %edi  # EDI
+    mov 28(%eax), %ebp  # EBP
     
     push %eax
    
-    mov 36(%eax), %eax #EFLAGS
+    mov 36(%eax), %eax  # EFLAGS
     
     push %eax
     popf
     pop %eax
     
-    mov 24(%eax), %esp #ESP
+    mov 24(%eax), %esp  # ESP
     
     push %eax
-    mov 40(%eax), %eax #CR3
+    mov 40(%eax), %eax  # CR3
     mov %eax, %cr3
     
     pop %eax
     push %eax
     
-    mov 32(%eax), %eax #EIP
-    xchg (%esp), %eax #We do not have any more registers to use as tmp storage
+    mov 32(%eax), %eax  # EIP
+    xchg (%esp), %eax   # We do not have any more registers to use as tmp storage
    
-    mov (%eax), %eax #EAX
+    mov (%eax), %eax    # EAX
     
-    ret #This ends all!
+    ret                 # This ends all!
