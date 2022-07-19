2943d791191ed07b405f1500284ef2c69c6d7c9a
Felipe Lunkes <felipenldev@gmail.com>
2022-07-19 10:12:24 -0300

    Create init.c

---
+++ kern/init.c
@@ -0,0 +1,14 @@
+#include <Lunix/kernel/kernel.h>
+#include <Lunix/kernel/isr.h>
+#include <Lunix/kernel/thread.h>
+#include <stdint.h>
+
+void init_LXmonitor();
+ 
+static void kernel_thread() {
+    
+    kprint("\nkernel_thread: PID 0 started");
+
+    init_LXmonitor();
+
+}
