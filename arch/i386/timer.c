fd0dfff5ed8aad53b23c937169beac7c5af6c223
Felipe Lunkes <felipenldev@gmail.com>
2022-07-19 09:39:17 -0300

    Update timer.c

--- arch/i386/timer.c
+++ arch/i386/timer.c
@@ -6,8 +6,11 @@
 uint32_t tick = 0;
 
 static void timer_callback(registers_t *regs) {
+    
     tick++;
+    
     UNUSED(regs);
+    
 }
 
 void init_timer(uint32_t freq) {
@@ -19,7 +22,7 @@ void init_timer(uint32_t freq) {
     register_interrupt_handler(IRQ0, timer_callback);
 
     /* Get the PIT value: hardware clock at 1193180 Hz */
-
+    
     uint32_t divisor = 1193180 / freq;
     uint8_t low  = (uint8_t)(divisor & 0xFF);
     uint8_t high = (uint8_t)( (divisor >> 8) & 0xFF);
@@ -29,6 +32,8 @@ void init_timer(uint32_t freq) {
     port_byte_out(0x43, 0x36); /* Command port */
     port_byte_out(0x40, low);
     port_byte_out(0x40, high);
+    
+    kprint("\nThe timer has been set to 120 MHz.");
     
 }
 
